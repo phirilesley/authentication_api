265270073f6ac6a852cb81c4e38dec19130d3f0b
phirilesley <phirilesley@outlook.com>
2024-05-29 13:22:21 +0200

    initial

---
+++ authenticationAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using authenticationAPI.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace authenticationAPI.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+ 
+
+    }
+}
